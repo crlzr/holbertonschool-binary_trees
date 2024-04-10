268b0fd02de9e82133b2bf957c4c7b95dc854ba2
Carole <c.zenruffinen@hotmail.com>
2024-04-10 06:09:53 +0000

    updating function

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -16,13 +16,8 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	{
 		return (NULL);
 	}
-	
-	if (node->parent->left == NULL || node->parent->right == NULL)
-	{
-		return (NULL);
-	}
 
-	if (node->parent->left != NULL)
+	if (node->parent->left == node)
 	{
 		return (node->parent->right);
 	}
